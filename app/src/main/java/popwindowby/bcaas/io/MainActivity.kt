77615bae0a2f5ad0a0a3495e93627a6a74c49649
catherineliu <catherineliu@orangeblock.tech>
2019-02-14 11:34:02 +0800

    弹出popwindow

--- app/src/main/java/popwindowby/bcaas/io/MainActivity.kt
+++ app/src/main/java/popwindowby/bcaas/io/MainActivity.kt
@@ -3,16 +3,27 @@ package popwindowby.bcaas.io
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.widget.PopupWindowCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * popWindow 不建議規定其大小，否則如果用戶調節了系統的字體就會容易造成顯示異常。
+ * 所以，應該在彈框前，重新得到PopWindow的大小
+ *
+ * https://www.jianshu.com/p/6c32889e6377
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener {
+        btnBAL.setOnClickListener {
             showBottomAndLeft()
+        }
+        btnUp.setOnClickListener {
+            showUpOrLeftOrRight()
         }
 
     }
@@ -23,10 +34,42 @@ class MainActivity : AppCompatActivity() {
     private fun showBottomAndLeft() {
         val popWindow = TestPopWindow(this)
         //根据指定view定位
-        PopupWindowCompat.showAsDropDown(popWindow, btn, 50, 0, Gravity.END)
+        PopupWindowCompat.showAsDropDown(popWindow, btnBAL, 50, 0, Gravity.END)
         // 或者
 //        popWindow.showAsDropDown(btn)
         //又或者使用showAtLocation 根据屏幕来定位
 //        popWindow.showAtLocation(window.decorView,Gravity.END,50,0)
     }
+
+    /**
+     * 重新結算popWindow的寬高
+     */
+    private fun makeDropDownMeasureSpec(measureSpec: Int): Int {
+        val mode: Int = if (measureSpec == ViewGroup.LayoutParams.WRAP_CONTENT)
+            View.MeasureSpec.UNSPECIFIED else View.MeasureSpec.EXACTLY
+        return View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.getSize(measureSpec), mode)
+    }
+
+    /**
+     * 弹出在视图的上面/左边/右边，因为不能直接写死popWindow的宽高，那么就需要重新量popWindow的的宽高
+     */
+    private fun showUpOrLeftOrRight() {
+        val popWindow = TestPopWindow(this)
+        popWindow.showBackGroundAnimator(0.7f)
+        popWindow.setOnDismissListener {         popWindow.showBackGroundAnimator(1f)
+        }
+        val contentView = popWindow.contentView
+        //need to measure first ,popWindow's width and height is Zero before it show
+        contentView.measure(makeDropDownMeasureSpec(popWindow.width), makeDropDownMeasureSpec(popWindow.height))
+        //You should show anyWhere after measured
+        //first，显示在左边，对等
+//        val offsetX = -popWindow.contentView.measuredWidth
+//        val offsetY = -popWindow.contentView.measuredHeight
+//        PopupWindowCompat.showAsDropDown(popWindow, btnUp, offsetX, offsetY, Gravity.START)
+        //Second 显示在上边
+        val offsetX = Math.abs(popWindow.contentView.measuredWidth - btnUp.width) / 2
+        val offsetY = -popWindow.contentView.measuredHeight-btnUp.height
+        PopupWindowCompat.showAsDropDown(popWindow, btnUp, offsetX, offsetY, Gravity.START)
+    }
+
 }
--- app/src/main/java/popwindowby/bcaas/io/TestPopWindow.kt
+++ app/src/main/java/popwindowby/bcaas/io/TestPopWindow.kt
@@ -1,10 +1,14 @@
 package popwindowby.bcaas.io
 
+import android.animation.ValueAnimator
+import android.app.Activity
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import android.widget.PopupWindow
 
 /**
@@ -17,8 +21,11 @@ import android.widget.PopupWindow
  *
  */
 class TestPopWindow : PopupWindow {
+    private var context: Context?
+//    private var alpha = 1f//背景灰度 0-1，1表示全透明
 
     constructor(context: Context) : super(context) {
+        this.context = context
         height = ViewGroup.LayoutParams.WRAP_CONTENT
         width = ViewGroup.LayoutParams.WRAP_CONTENT
         isOutsideTouchable = true
@@ -28,5 +35,32 @@ class TestPopWindow : PopupWindow {
             R.layout.pop_test,
             null, false
         )
+    }
+
+    /**
+     * 控制窗口背景的不透明度
+     */
+    private fun setWindowBackgroundAlpha(alpha: Float) {
+        //if the context is null,should let it return
+        context ?: return
+        if (context is Activity) {
+            val window: Window = (context as Activity).window
+            val layoutParams: WindowManager.LayoutParams = window.attributes
+            layoutParams.alpha = alpha
+            window.attributes = layoutParams
+        }
+    }
+
+    /**
+     * 窗口显示，窗口背景透明度渐变动画
+     */
+    fun showBackGroundAnimator(alpha: Float) {
+        if (alpha >= 1f) return
+        val animator: ValueAnimator = ValueAnimator.ofFloat(1.0f, alpha)
+        animator.addUpdateListener {
+            setWindowBackgroundAlpha(animator.animatedValue as Float)
+        }
+        animator.duration = 360
+        animator.start()
     }
 }
